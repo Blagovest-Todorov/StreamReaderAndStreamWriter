c451091ae4483e4d1842f532a1c461c0d993d045
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-22 19:44:52 +0200

    try cathc small example with StreamReader and StreamWriter

--- Program.cs
+++ Program.cs
@@ -12,34 +12,50 @@ namespace StreamReadWriter
     {
         static void Main()
         {
-
-
-            var reader = new StreamReader(@"C:\Users\Bla\source\repos\TextFiles\ReadingFileWritingToFile\bin\Debug\netcoreapp3.1\FileReading.txt");
-            var writer = new StreamWriter(@"C:\Users\Bla\source\repos\TextFiles\ReadingFileWritingToFile\bin\Debug\netcoreapp3.1\FileWriting.txt");
-
-            using (reader)
+            try
             {
-                string currentLine = reader.ReadLine();
-                int count = 1;
-                using (writer)
+
+                var reader = new StreamReader(@"C:\Users\Bla\source\repos\TextFiles\ReadingFileWritingToFile\bin\Debug\netcoreapp3.1\FileReadingatxt");
+                var writer = new StreamWriter(@"C:\Users\Bla\source\repos\TextFiles\ReadingFileWritingToFile\bin\Debug\netcoreapp3.1\FileWriting.txt");
+
+                using (reader)
                 {
-
-                    while (currentLine != null)
+                    string currentLine = reader.ReadLine();
+                    int count = 1;
+                    using (writer)
                     {
-                        writer.WriteLine("{0:D2}. {1}", count, currentLine);
-                        count++;
-                        currentLine = reader.ReadLine();
-                        Console.WriteLine(currentLine);
-                        Console.WriteLine("{0:D2}. {1}", count, currentLine);
-                    }//We can not at the same time read form file and write into the same file ..throws an exception//Principle of concurency
-                    //many users may read from one file, gut at the same time writiung is not allowed/ 
+
+                        while (currentLine != null)
+                        {
+                            writer.WriteLine("{0:D2}. {1}", count, currentLine);
+                            count++;
+                            currentLine = reader.ReadLine();
+                            // Console.WriteLine(currentLine);
+                            // Console.WriteLine("{0:D2}. {1}", count, currentLine);
+                        }//We can not at the same time read form file and write into the same file ..throws an exception//Principle of concurency
+                         //many users may read from one file, gut at the same time writiung is not allowed/ 
+                    }
+
+
                 }
-                
-               
+
             }
+            catch (FileNotFoundException)
+            {
+
+                Console.WriteLine("the File is not found. Try gain :  FileReadinga.txt "); ;
+            }
+            //Streams are the main Iput/Output/ mechanism of .Net
+            //The StreamReader Class and the ReadLine() method are used to read text files
+            //the StreamWriter class and the WriteLine() methosd are used to write test files
+            //Exceptions are unusual events or erros conditions
+            //Exceptions can be handeled by try-catch finally blocks
 
 
-            
+
+
+
+
         }
     }
 }
